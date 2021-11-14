341ca3d8c49583aa7814cfbe0ccbaa492a10e649
almasbek uulu iman <iman.almasbekuulu.kk@gmail.com>
2021-11-14 21:41:12 +0300

    add models.py toDo and toMeet

--- main/models.py
+++ main/models.py
@@ -1,3 +1,15 @@
+from typing import Text
 from django.db import models
 
-# Create your models here.
+class ToDo(models.Model):
+    text = models.CharField(max_length=100)
+    created_at = models.DateField(auto_now_add=True)
+    is_closed = models.BooleanField(default=False)
+    is_favorite = models.BooleanField(default=False)
+
+class toMeet(models.Model):
+    persone = models.CharField(max_length=20)
+    phone_number = models.CharField(max_length=12)
+    date_of_meeting = models.DateTimeField(auto_now=True)
+    is_closed = models.BooleanField(default=False)
+    is_favorite = models.BooleanField(default=False)
