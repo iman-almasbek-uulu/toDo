43709d927db8897a0cc76327dcecefff715dbb24
almasbek uulu iman <iman.almasbekuulu.kk@gmail.com>
2021-11-14 23:27:23 +0300

    added meeting.html

--- main/views.py
+++ main/views.py
@@ -1,6 +1,7 @@
 from django.http import request
 from django.shortcuts import render, HttpResponse
-from .models import ToDo
+from .models import ToDo 
+from .models import toMeet
 
 
 def homepage(request):
@@ -12,6 +13,7 @@ def test(request):
     todo_list = ToDo.objects.all()
     return render(request, "test.html",{"todo_list": todo_list})
 
-def second(request):
-    return HttpResponse("my name is Iman!")
+def meeting(request):
+    tomeet_list = toMeet.objects.all()
+    return render(request, "meeting.html", {"tomeet_list": tomeet_list})
 
